fd16a163a22f3d4bfc016b16f7ffe4ddbdadfa8c
codeagha <amirmahdyjdx@gmail.com>
2025-10-24 18:00:04 +0330

    init project

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="w-full h-screen px-4 py-2 overflow-x-hidden flex items-center justify-center">
+    <RouterView />
+  </div>
+</template>
