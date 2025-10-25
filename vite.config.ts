77bc3ab111ab7a897a25523451398ecd9752ce72
codeagha <amirmahdyjdx@gmail.com>
2025-10-26 02:20:32 +0330

    fix: vite config base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import svgLoader from 'vite-svg-loader'
 
+const isCi = Boolean(process.env.GITHUB_ACTIONS)
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+const base = process.env.VITE_BASE ?? (isCi && repoName ? `/${repoName}/` : './')
 
-// https://vite.dev/config/
 export default defineConfig({
+  base,
   plugins: [
     vue(),
     vueDevTools(),
     svgLoader()
   ],
+  server: {
+    allowedHosts: ["amirmahdyjebreily.github.io"],
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
