46abd23d10ee9c0247f13c970af96efa523799db
Code Agha <amirmahdyjdx@gmail.com>
2025-11-14 17:24:51 +0330

    fix: styles

--- src/views/home-view.vue
+++ src/views/home-view.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, onMounted, useTemplateRef } from 'vue'
+import { computed, useTemplateRef } from 'vue'
 import { useWeatherStore } from '@/stores/weather'
 import BaseCityCard from '@/components/base-city-card/base-city-card.vue'
 import { MapPinIcon, TrashIcon } from '@heroicons/vue/24/outline'
@@ -11,9 +11,9 @@ const store = useWeatherStore()
 const el = useTemplateRef('scroller')
 
 enum HomePageScrollShrinks {
-  MAX_HEIGHT = 200,
-  MIN_HEIGHT = 75,
-  MAX_SCROLL = 100,
+  MAX_HEIGHT = 230,
+  MIN_HEIGHT = 70,
+  MAX_SCROLL = (MAX_HEIGHT + MIN_HEIGHT) / 4,
 }
 
 const { height } = useScrollShrink(
@@ -26,54 +26,43 @@ const { height } = useScrollShrink(
 const headerStyle = computed(() => ({
   height: height.value ? height.value.toFixed(2) + 'px' : '100%',
 }))
-
-let timeout = -1
-onMounted(async () => {
-  if (el.value) {
-    el.value.addEventListener('scroll', () => {
-      el.value!.classList.add('scrollbar-visible')
-      clearTimeout(timeout)
-      timeout = setTimeout(() => {
-        el.value!.classList.remove('scrollbar-visible')
-      }, 800)
-    })
-  }
-})
 </script>
 
 <template>
   <header
     v-if="store.favorites.length > 0"
-    class="w-full container flex-col flex gap-2 items-center will-change-scroll justify-center pb-4"
+    class="w-full container flex-col flex gap-2 items-center will-change-scroll justify-center"
     :style="headerStyle"
   >
-    <h1
-      class="header-title text-2xl sm:text-3xl font-extrabold text-slate-700 [container-type:height] [&:container(height>150px)]:text-4xl [&:container(height>200px)]:text-5xl"
+    <div
+      class="header-container w-full container flex-row flex gap-2 items-center will-change-scroll justify-start pb-4 px-4"
     >
-      هوا چطوره ؟
-    </h1>
-    <h2
-      class="header-time text-lg sm:text-xl font-medium text-slate-600 [container-type:height] [&:container(height>150px)]:text-2xl [&:container(height>200px)]:text-3xl"
-    >
-      {{
-        new Date().toLocaleDateString('fa-IR', {
-          weekday: 'long',
-          day: '2-digit',
-          month: 'long',
-        })
-      }}، ساعت
-      {{
-        new Date().toLocaleTimeString('fa-IR', {
-          hour: '2-digit',
-          minute: '2-digit',
-        })
-      }}
-    </h2>
+      <h1
+        class="header-title text-right text-2xl sm:text-3xl font-extrabold text-slate-700 [container-type:height] [&:container(height>150px)]:text-4xl [&:container(height>200px)]:text-5xl"
+      >
+        هوا چطوره ؟
+      </h1>
+      <h2 class="header-time text-right text-lg sm:text-xl font-medium text-slate-600">
+        {{
+          new Date().toLocaleDateString('fa-IR', {
+            weekday: 'long',
+            day: '2-digit',
+            month: 'long',
+          })
+        }}، ساعت
+        {{
+          new Date().toLocaleTimeString('fa-IR', {
+            hour: '2-digit',
+            minute: '2-digit',
+          })
+        }}
+      </h2>
+    </div>
   </header>
 
   <div
     ref="scroller"
-    class="w-full flex flex-col scroll-py-2 items-center justify-start bg-gray-200 gap-3 flex-1 h-full overflow-auto p-4 scrollbar-mobile rounded-3xl border-8 border-slate-200"
+    class="w-full flex flex-col scroll-py-2 items-center justify-start bg-gray-50 gap-3 flex-1 h-full overflow-auto px-2 py-5 rounded-3xl border border-gray-200"
   >
     <BaseCityCard
       v-for="value in store.favorites"
@@ -128,18 +117,24 @@ header {
 .header-time {
   font-size: 1rem;
   transition:
-    font-size 0.2s ease,
-    margin 0.2s ease;
+    font-size 0.15s ease,
+    margin 0.25s ease;
 }
 
 /* وقتی ارتفاع container حداقل 100px باشد */
 @container (min-height: 100px) {
+  .header-container {
+    flex-direction: column;
+    justify-content: center;
+  }
   .header-title {
     font-size: 2rem;
+    text-align: right !important;
   }
 
   .header-time {
     font-size: 1.25rem;
+    text-align: right !important;
   }
 }
 
@@ -147,21 +142,28 @@ header {
 @container (min-height: 150px) {
   .header-title {
     font-size: 2.5rem;
+    text-align: center !important;
   }
 
   .header-time {
     font-size: 1.5rem;
+    text-align: center !important;
   }
 }
 
 /* وقتی ارتفاع container حداقل 200px باشد */
 @container (min-height: 200px) {
+  .header-container {
+    gap: 1.5rem;
+  }
   .header-title {
     font-size: 3rem;
+    text-align: center !important;
   }
 
   .header-time {
     font-size: 1.75rem;
+    text-align: center !important;
   }
 }
 </style>
